8d16ab41b4c26296d1b7a3bf792364120a9d9bda
Artur Piernikarczyk <arturdomek@wp.pl>
2014-04-20 16:18:37 +0200

    add sh script for easy use

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This is a script for running voice recognition from microphone.
+#Just run in terminal, and adjust your mic input volume.
+#Make sure that mic input level is not too high.
+
+./julius_engine/julius -C julius_engine/Sample.jconf
+
+
